b93c66c67ce9a71ae17a4dccc6619edad08b3736
SMILE\30369 <3036951769@qq.com>
2024-05-04 11:26:00 +0800

    first commit

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,15 @@ two required: route and view
 two optional: kwargs and name
 """
 #url configuration,put views.index in URLconf
-urlpatterns = [path("",views.index,name="index"),]
+#To associate URLs with views, Django uses 'URLconfs' to configure
+
+urlpatterns = [
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/5/    question id is 5
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+"""
+Create your views here.
+The concept of a view in Django is 
+              "a collection of web pages with the same functionality and templates."
+
+"""
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
