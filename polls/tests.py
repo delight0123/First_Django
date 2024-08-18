07cc019a74a4fb9482820fcec85842e445dd530e
SMILE\30369 <3036951769@qq.com>
2024-08-18 19:17:18 +0800

    new

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+# 按照惯例，Django 应用的测试应该写在应用的 tests.py 文件里。
+# 测试系统会自动的在所有文件里寻找并执行以 test 开头的测试函数
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        bug：将来发布的问题应该返回 False
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
--- polls/urls.py
+++ polls/urls.py
@@ -10,13 +10,20 @@ two optional: kwargs and name
 
 app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
+    # # ex: /polls/
+    # path("", views.index, name="index"),
     
-    # ex: /polls/5/    question id is 5
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
+    # # ex: /polls/5/    question id is 5
+    # path("<int:question_id>/", views.detail, name="detail"),
+    # # ex: /polls/5/results/
+    # path("<int:question_id>/results/", views.results, name="results"),
+    # # ex: /polls/5/vote/
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
+    #第二和第三个模式的路径字符串中匹配的模式名称已从 <question_id> 更改为 <pk>。
+    # 使用 DetailView 通用视图
+    # 来替换detail() 和 results() 视图 (二者原先的视图除了模板名字一模一样。为了解决冗余，使用通用视图)，它期望从 URL 中捕获的主键值被称为 "pk"。
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
